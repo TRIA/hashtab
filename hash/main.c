4bca4c728940b1fd5de4c30e399dea5180229059
Steve Bunch <steve.bunch@gmail.com>
2022-10-14 15:05:03 -0400

    Initial Commit

---
+++ hash/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hash
+//
+//  Created by Steve Bunch on 10/14/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
